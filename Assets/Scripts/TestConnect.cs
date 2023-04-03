7a1ac627437b7fafa8fee1a0bbba2ae1cf207972
DiamVanOort <113904997+DiamVanOort@users.noreply.github.com>
2023-04-03 13:01:22 +0100

    Server setup complete

---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    public GameObject playerPrefab;
+    private void Start()
+    {
+        print("Connecting to server");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() 
+    {
+        print("Connected to server");
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from server for reason " + cause.ToString());
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("Joind a room succesfully");
+        PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+    }
+}
