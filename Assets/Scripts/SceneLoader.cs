b34d1bdfce59d5f99e3067eaadaa6025288302a7
DiamVanOort <113904997+DiamVanOort@users.noreply.github.com>
2023-04-18 13:13:15 +0100

    scene switch werkt

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Windows;
 
 public class SceneLoader : MonoBehaviour
 {
@@ -12,12 +13,61 @@ public class SceneLoader : MonoBehaviour
     void Start()
     {
 
-        string Query = "diam=cool&1234=QWER"; //Application.absoluteURL.Split('?')[1];
+        string Query = "sceneID=1&1234=QWER"; //Application.absoluteURL.Split('?')[1];
+
+/*#if UNITY_WEBGL
+        Query = Application.absoluteURL.Split('?')[1];
+#endif*/
+
+
         string[] Values = Query.Split('&');
         string[] val1 = Values[0].Split('=');
         string[] val2 = Values[1].Split('=');
-        SceneManager.LoadScene(val1[1]);
         string UniqueKey = val2[1];
+
+        int sceneID = 0;
+
+        try { sceneID = Int32.Parse(val1[1]); }
+        catch { Debug.Log("not an int");}
+        
+
+        switch (sceneID) 
+        {
+            case 1:
+                {
+                    SceneManager.LoadScene("Diorama_Chris");
+                    break;
+                }
+            case 2:
+                {
+                    SceneManager.LoadScene("Diorama_Chris_2");
+                    break;
+                }
+            case 3: 
+                {
+                    SceneManager.LoadScene("Diorama_Verena");
+                    break;
+                }
+            case 4:
+                { 
+                    SceneManager.LoadScene("Diorama_Verena_2");
+                    break;
+                }
+            case 5:
+                {
+                    SceneManager.LoadScene("Diorama_hellen");
+                    break;
+                }
+            case 6: 
+                {
+                    SceneManager.LoadScene("Diorama_Jeppe");
+                    break;
+                }
+            default:
+                Debug.Log("No scene found " + val1[1]);
+                break;
+        
+        }
         int x = 0;
     }
 
